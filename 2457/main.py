a3fce1180de6a9f35e6a0aad8e4d259a8f67a73a
Universe1122 <whdals7979@gmail.com>
2021-12-26 20:25:58 +0900

    2021.12.26

---
+++ 2457/main.py
@@ -0,0 +1,12 @@
+# import sys
+
+# flower_cnt = int(sys.stdin.readline())
+# flower_info = list()
+
+# for _ in range(flower_cnt):
+#     info = list(map(int, sys.stdin.readline().split(" ")))
+#     flower_info.append([info[0] * 100 + info[1], info[2] * 100 + info[3]])
+
+# flower_info.sort()
+# max_date 
+# while True:
