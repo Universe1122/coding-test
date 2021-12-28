151f089e8015fc772296aaea09d26130ba211873
Universe1122 <whdals7979@gmail.com>
2021-12-28 13:44:00 +0900

    2021.12.28

--- 2457/main.py
+++ 2457/main.py
@@ -1,12 +1,40 @@
-# import sys
+import sys
 
-# flower_cnt = int(sys.stdin.readline())
-# flower_info = list()
+flower_cnt = int(sys.stdin.readline())
+flower_info = list()
 
-# for _ in range(flower_cnt):
-#     info = list(map(int, sys.stdin.readline().split(" ")))
-#     flower_info.append([info[0] * 100 + info[1], info[2] * 100 + info[3]])
+for _ in range(flower_cnt):
+    info = list(map(int, sys.stdin.readline().split(" ")))
+    flower_info.append([info[0] * 100 + info[1], info[2] * 100 + info[3]])
 
-# flower_info.sort()
-# max_date 
-# while True:
+max_date = 0
+check = 0
+count = 0
+for info in flower_info:
+    if info[0] <= 301:
+        if max_date < info[1]:
+            max_date = info[1]
+            check = 1
+
+if check:
+    count += 1
+else:
+    print(0)
+    exit()
+
+while max_date < 1201:
+    check = 0
+    start_day = max_date
+    for info in flower_info:
+        if info[0] > 301 and info[0] <= start_day:
+            if max_date < info[1]:
+                max_date = info[1]
+                check = 1
+        
+    if check:
+        count += 1
+    else:
+        count = 0
+        break
+
+print(count)
